6b824ca2f17be46a8250ff897bbd26b19c8a146b
Igor Urbanowicz <115647685+IgorUrbanowicz01@users.noreply.github.com>
2023-08-09 19:45:13 +0200

    perceptron full implementation

--- perceptron.py
+++ perceptron.py
@@ -1,9 +1,29 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
+def plot_decision_region(X, y, classifier, resoloution=0.02):
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'grey', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min()-1, X[:, 0].max()+1
+    x2_min, x2_max = X[:, 1].min()-1, X[:, 1].max()+1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resoloution),
+                           np.arange(x2_min, x2_max, resoloution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=colors[idx],
+                    marker=markers[idx], label=cl, edgecolors='black')
 class Perceptron(object):
 
-
-    def __int__(self, eta = 0.01, n_iter=50, random_state=0):
+    def __init__(self, eta=0.01, n_iter=50, random_state=0):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
@@ -18,7 +38,7 @@ class Perceptron(object):
             errors = 0
             for xi, target in zip(X, y):
                 update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += update *xi
+                self.w_[1:] += update * xi
                 self.w_[0] += update
                 errors += int(update != 0.0)
             self.errors_.append(errors)
@@ -33,5 +53,29 @@ class Perceptron(object):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+    y = df.iloc[0:100, 4].values
+    y = np.where(y == 'Iris-setosa', -1, 1)
+    X = df.iloc[0:100, [0, 2]].values
 
-    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='Setosa')
+    plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='Versioncolor')
+    plt.xlabel('Długość działki [cm]')
+    plt.xlabel('Długość płatka [cm]')
+    plt.legend(loc='upper left')
+    plt.show()
+
+    ppn = Perceptron(eta=0.1, n_iter=10)
+    ppn.fit(X,y)
+    plt.clf()
+    plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+    plt.xlabel('Epoki')
+    plt.ylabel('Liczba aktualizacji')
+    plt.show()
+
+    plt.clf()
+    plot_decision_region(X, y, classifier=ppn)
+    plt.xlabel('Długość działki [cm]')
+    plt.xlabel('Długość płatka [cm]')
+    plt.legend(loc='upper left')
+    plt.show()
