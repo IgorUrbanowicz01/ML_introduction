60dd65e0b17ef94b7934e31cd4636bbfe2b15546
Igor Urbanowicz <115647685+IgorUrbanowicz01@users.noreply.github.com>
2023-08-29 20:28:59 +0200

    Pipe line

--- k-cross-validation.py
+++ k-cross-validation.py
@@ -13,4 +13,10 @@ if __name__ == '__main__':
     y = le.fit_transform(y)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1, stratify=y)
 
-    pipe_lr =
+    pipe_lr = make_pipeline(StandardScaler(),
+                            PCA(n_components=2),
+                            LogisticRegression(random_state=1))
+    pipe_lr.fit(X_train, y_train)
+    y_pred = pipe_lr.predict(X_test)
+
+    print('Dokładność testu: %.3f' % pipe_lr.score(X_test, y_test))
