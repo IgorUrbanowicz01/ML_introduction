4241c525909aeb70e5164fe0731fa31b20a11fad
Igor Urbanowicz <115647685+IgorUrbanowicz01@users.noreply.github.com>
2023-08-29 20:18:22 +0200

    Pipe line init

---
+++ k-cross-validation.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+
+if __name__ == '__main__':
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header=None)
+    le = LabelEncoder()
+    X = df.loc[:, 2:].values
+    y = df.loc[:, 1].values
+    y = le.fit_transform(y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1, stratify=y)
+
+    pipe_lr =
