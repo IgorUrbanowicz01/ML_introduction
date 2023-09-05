5125421ab30b9fada8b61dd28088522fec469096
Igor Urbanowicz <115647685+IgorUrbanowicz01@users.noreply.github.com>
2023-09-05 21:33:00 +0200

    add scatter polt matrix

--- linera-regretion.py
+++ linera-regretion.py
@@ -1,13 +1,17 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 if __name__ == '__main__':
+
     df = pd.read_csv('https://raw.githubusercontent.com/rasbt/'
                      'python-machine-learning-book-2nd-edition'
                      '/master/code/ch10/housing.data.txt',
                      header=None,
                      sep='\s+')
-
     df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS',
                   'NOX', 'RM', 'AGE', 'DIS', 'RAD',
                   'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
-    df.head()
+    cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
+    sns.pairplot(df[cols], height=2.5)
+    plt.tight_layout()
+    plt.show()
